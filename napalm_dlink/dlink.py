8cadbf4c4b73b238ef68cf578bc6eb95d9822110
Nikolay Chobanov <hromus@gmail.com>
2020-01-15 09:01:02 +0300

    add base methods: open, close, etc.

--- napalm_dlink/dlink.py
+++ napalm_dlink/dlink.py
@@ -18,6 +18,8 @@ Napalm driver for Dlink.
 
 Read https://napalm.readthedocs.io for more information.
 """
+import socket
+import telnetlib
 
 from napalm.base import NetworkDriver
 from napalm.base.exceptions import (
@@ -26,14 +28,16 @@ from napalm.base.exceptions import (
     MergeConfigException,
     ReplaceConfigException,
     CommandErrorException,
+    ConnectionClosedException
 )
+from napalm.base.netmiko_helpers import netmiko_args
 
 
 class DlinkDriver(NetworkDriver):
     """Napalm driver for Dlink."""
 
     def __init__(self, hostname, username, password, timeout=60, optional_args=None):
-        """Constructor."""
+        """NAPALM Dlink Handler.."""
         self.device = None
         self.hostname = hostname
         self.username = username
@@ -43,11 +47,66 @@ class DlinkDriver(NetworkDriver):
         if optional_args is None:
             optional_args = {}
 
-    def open(self):
-        """Implement the NAPALM method open (mandatory)"""
-        pass
+        self.transport = optional_args.get("transport", "ssh")
 
+        if self.transport == "telnet":
+            # Telnet only supports inline_transfer
+            self.inline_transfer = True
+
+        self.netmiko_optional_args = netmiko_args(optional_args)
+
+        # Set the default port if not set
+        default_port = {"ssh": 22, "telnet": 23}
+        self.netmiko_optional_args.setdefault("port", default_port[self.transport])
+
+    def open(self):
+        """Open a connection to the device."""
+        device_type = "dlink_ds"
+        if self.transport == "telnet":
+            device_type = "dlink_ds_telnet"
+        self.device = self._netmiko_open(
+            device_type, netmiko_optional_args=self.netmiko_optional_args
+        )
 
     def close(self):
-        """Implement the NAPALM method close (mandatory)"""
-        pass
+        """Close the connection to the device and do the necessary cleanup."""
+        self._netmiko_close()
+
+    def _send_command(self, command):
+        """Wrapper for self.device.send.command().
+        If command is a list will iterate through commands until valid command.
+        """
+        try:
+            if isinstance(command, list):
+                for cmd in command:
+                    output = self.device.send_command(cmd)
+                    if "Available commands" not in output and "Next possible" not in output:
+                        break
+            else:
+                output = self.device.send_command(command)
+            return output
+        except (socket.error, EOFError) as e:
+            raise ConnectionClosedException(str(e))
+
+    def is_alive(self):
+        """Returns a flag with the state of the connection."""
+        null = chr(0)
+        if self.device is None:
+            return {"is_alive": False}
+        if self.transport == "telnet":
+            try:
+                # Try sending IAC + NOP (IAC is telnet way of sending command
+                # IAC = Interpret as Command (it comes before the NOP)
+                self.device.write_channel(telnetlib.IAC + telnetlib.NOP)
+                return {"is_alive": True}
+            except AttributeError:
+                return {"is_alive": False}
+        else:
+            # SSH
+            try:
+                # Try sending ASCII null byte to maintain the connection alive
+                self.device.write_channel(null)
+                return {"is_alive": self.device.remote_conn.transport.is_active()}
+            except (socket.error, EOFError):
+                # If unable to send, we can tell for sure that the connection is unusable
+                return {"is_alive": False}
